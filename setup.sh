21d9bf1da3a06b23ecd7377bb475a3a5f22dba24
Rupesh <76819843+Rup22784esh@users.noreply.github.com>
2020-12-31 22:45:09 +0545

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,13 @@
 apt update && apt upgrade
+pkg install python -y
+pkg install python2 -y
+pkg install bash -y
+pkg install ruby -y
+gem install lolcat -y
+pkg install figlet -y
+pkg install toilet -y
+pkg install openssh -y
+pkg install php -y
+pkg install git -y
+pkg install wget -y
+pkg install nano -y
