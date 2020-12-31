40cb1ac9ea223567468244a2a2c4a3a3d1a5434e
Rupesh <76819843+Rup22784esh@users.noreply.github.com>
2021-01-01 02:31:34 +0545

    Create first.sh

---
+++ first.sh
@@ -0,0 +1,49 @@
+
+
+cd
+
+cd setup
+cp .zshrc $HOME -y
+cd
+git clone https://github.com/sparkz-technology/Target.git
+
+cd 
+
+
+apt-get install python2
+apt-get install git
+git clone https://github.com/AngelSecurityTeam/BluForce-FB
+pip2 install mechanize
+
+apt update
+apt install git python python2 -y
+git clone https://github.com/htr-tech/afgcrack.git
+
+git clone https://github.com/Mr-NemrMedo/fb-group-hack
+
+
+git clone https://github.com/Manisso/fsociety
+
+apt install git curl php wget -y
+git clone git://github.com/htr-tech/zphisher.git
+
+git clone https://github.com/xHak9x/fbi.git
+
+
+git clone https://github.com/reyspeed/phish-all
+
+
+
+git clone https://github.com/mkdirlove/FBTOOL
+
+
+
+git clone https://github.com/sparkz_technology/hacklock
+
+git clone https://github.com/TheSpeedX/TBomb
+
+
+
+git clone https://github.com/kashigangster/CrackFb.git
+
+git clone https://github.com/sparkz-technology/hackcam
